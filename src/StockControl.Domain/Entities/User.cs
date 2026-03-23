8637ec5488ae83cb45c71545ee268e8c2aaf9540
Carlossnogueira <carlosnogueira372@gmail.com>
2026-03-23 20:45:30 -0300

    feat: adiciona feat: adiciona classe de dominio User

---
+++ src/StockControl.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using StockControl.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockControl.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } = string.Empty;
+        public Role Role { get; set; } = Role.Employee;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
